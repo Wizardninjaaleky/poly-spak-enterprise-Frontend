4908fe64a27f4e629cc026a9806e281186d49c54
Wizardninjaaleky <alexnyakundi56@gmail.com>
2025-11-27 14:49:49 +0300

    Fix login mapping in authRoutes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser, loginUser, createAdmin } from '../controllers/authController.js';
+import { registerUser as register, loginUser as login, createAdmin } from '../controllers/authController.js';
 
 const router = express.Router();
 
